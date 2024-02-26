fcbcfb2d995a06341b16ea4ebc1a11f905d4edb3
Alina Ananieva <bwldthesis@gmail.com>
2024-02-26 01:25:27 +0000

    first commit§

---
+++ functions.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int nutcracker()
+{
+    int n, k, nut;
+    cin >> n >> k;
+    nut = k / n;
+    cout << nut << endl;
+    return 0;
+}
+
+int nutcracker_2()
+{
+    int n, k, num;
+    cin >> n >> k;
+    num = k % n;
+    cout << num << endl;
+    return 0;
+}
+
+int last_digit()
+{
+    int n, result;
+    cin >> n;
+    result = n % 10;
+    cout << result << endl;
+    return 0;
+}
+
+int num_ten()
+{
+    int n;
+    int result;
+    cin >> n;
+    result = n / 10;
+    cout << result << endl;
+    return 0;
+}
+
+int penultimate_digit()
+{
+    int n, result;
+    cin >> n;
+    result = (n % 100) / 10;
+    cout << result << endl;
+    return 0;
+}
+
+int digit_sum()
+{
+    int n, result;
+    cin >> n;
+    result = (n / 100) + (n % 10) + (n % 100) / 10;
+    cout << result << endl;
+    return 0;
+}
+
+int next_even()
+{
+    int n, result;
+    cin >> n;
+    result = n + 2 - (n % 2);
+    cout << result << endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int nutcracker();
+int nutcracker_2();
+int last_digit();
+int num_ten();
+int penultimate_digit();
+int digit_sum();
+int next_even();
+
+
+
+int main() {
+    cout << "Hello!" << endl;
+    cout << "Let's go some non-trivial math tasks in c++";
+    nutcracker();
+    nutcracker_2();
+    last_digit();
+    num_ten();
+    penultimate_digit();
+    digit_sum();
+    next_even();
+    return 0;
+}
